e96b9402476d912266025224f616a5a074812fda
Braedon <43808091+Incompetentexercise@users.noreply.github.com>
2020-04-27 15:40:16 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 
 fonts = {
-    "heading": ("Arial", "16", "bold")
+    "heading": ("Arial", "16", "bold"),
+    "body": ('Arial', '10')
 }
+help_text = "First select a temperature to convert from by clicking one of the unit buttons on the far left.\n\n" \
+            "Then input a temperature into the enabled text box.\n\n" \
+            "Take care not to enter a temperature below absolute zero.\n\n" \
+            "After this, press the convert button to see the converted values."
 
 
 class Converter:
@@ -33,7 +38,21 @@ class Help:
     def __init__(self, parent):
         self.parent = parent
         self.root = tk.Toplevel(master=self.parent.root)
+        self.root.title("Conversion Help")
+        self.root.geometry("350x300")
+        # bind the window close button to the close function in this class
+        # important for enabling the help button
         self.root.protocol('WM_DELETE_WINDOW', self.close)
+
+        self.main_label = tk.Label(
+            self.root,
+            text=help_text,
+            font=fonts["body"],
+            padx=10, pady=10,
+            wraplength=320,
+            justify=tk.LEFT
+            )
+        self.main_label.grid(row=10, column=10)
 
     def close(self):
         self.parent.help_button.config(state=tk.NORMAL)
